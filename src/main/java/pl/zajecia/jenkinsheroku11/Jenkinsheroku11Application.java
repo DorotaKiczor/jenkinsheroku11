ea1fe9293910c7da6693dc2adc980231dd15cb0c
DorotaKiczor <dorokic980@student.polsl.pl>
2022-04-20 14:43:53 +0200

    gy

--- src/main/java/pl/zajecia/jenkinsheroku11/Jenkinsheroku11Application.java
+++ src/main/java/pl/zajecia/jenkinsheroku11/Jenkinsheroku11Application.java
@@ -3,7 +3,9 @@ package pl.zajecia.jenkinsheroku11;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Jenkinsheroku11Application {
 
