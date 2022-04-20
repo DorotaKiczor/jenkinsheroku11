c7e146887ce75a0c462faf34ab362bca2292d9ef
DorotaKiczor <dorokic980@student.polsl.pl>
2022-04-20 13:49:44 +0200

    dr

---
+++ src/main/java/pl/zajecia/jenkinsheroku11/Jenkinsheroku11Application.java
@@ -0,0 +1,19 @@
+package pl.zajecia.jenkinsheroku11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Jenkinsheroku11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkinsheroku11Application.class, args);
+	}
+
+	@GetMapping
+	public String get()
+	{
+		return "Hello";
+	}
+}
